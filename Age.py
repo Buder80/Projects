ce7407059f8ba58965529a3f2835804a1c4cc744
Buder80 <buder@wp.pl>
2022-09-25 20:30:14 +0100

    Age check

---
+++ Age.py
@@ -0,0 +1,22 @@
+#Przemyslaw Budrewicz
+#25/09/2022
+#Age check
+
+num = 0 #loop one try is not nice :)
+
+while num < 5:
+
+    age = int(input("How old You are?"))
+    print(age)
+
+    if age >= 18:
+        print("You can vote.")
+    elif age == 17:
+        print("You can learn to drive.")
+    elif age == 16:
+        print("You van buy lottery ticket.")
+    elif age < 16:
+        print("You can go trick or treating.")
+    num = num +1
+
+print("Game Over!")
